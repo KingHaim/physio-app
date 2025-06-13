a5b24608ff5f9299bca3602e4c376312534a0472
Haim <haim@example.com>
2025-06-13 23:59:35 +0200

    fix: merge migration heads

---
+++ migrations/versions/f071be6da536_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: f071be6da536
+Revises: 5ad53f2be9cb, a9ca7a251865
+Create Date: 2025-06-13 23:59:31.101996
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f071be6da536'
+down_revision = ('5ad53f2be9cb', 'a9ca7a251865')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
